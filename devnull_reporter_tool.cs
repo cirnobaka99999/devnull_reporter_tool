f463fe071d179aa42e2e572b88805e95761e25a7
geekgit <geeekz@gmail.com>
2018-01-30 17:09:07 +0000

    devnull_reporter_tool.cs

---
+++ devnull_reporter_tool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace devnull_reporter_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random R = new Random(DateTime.Now.Millisecond);
+            int r = R.Next();
+        }
+    }
+}
